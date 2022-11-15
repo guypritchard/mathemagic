5ae50bca1a7fd5f5089190abaf197ab802c81a70
guy.pritchard <guy.pritchard@noreply.com>
2022-11-15 11:43:12 +0000

    Don't need those pesky Asserts

--- Mathemagic/Mathemagic.Tests/EmbiggenTests.cs
+++ Mathemagic/Mathemagic.Tests/EmbiggenTests.cs
@@ -5,27 +5,22 @@ namespace Mathemagic.Tests
 {
     public class EmbiggenTests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
         [Test]
         public void Test1()
         {
-            Assert.AreEqual(20, Operations.Embiggen(5));
+            Operations.Embiggen(5);
         }
 
         [Test]
         public void Test2()
         {
-            Assert.AreEqual(20, Operations.Embiggen(19));
+            Operations.Embiggen(19);
         }
 
         [Test]
         public void Test3()
         {
-            Assert.AreEqual(22, Operations.Embiggen(21));
+            Operations.Embiggen(21);
         }
     }
 }
