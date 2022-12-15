b83edca72d8260408778dcc89b2196517ffe00d4
guy.pritchard <guy.pritchard@noreply.com>
2022-12-15 18:15:06 +0000

    Testing

--- Mathemagic/Mathemagic.Core/Operations.cs
+++ Mathemagic/Mathemagic.Core/Operations.cs
@@ -4,11 +4,18 @@
     {
         public static int Add(int x, int y)
         {
-            return 2 + y;
+            if (x < 0 || y < 0)
+                throw new ArgumentOutOfRangeException("inputs");
+
+            var result = 2 + y;
+            return result;
         }
 
         public static int Embiggen(int x)
         {
+            if (x < 0)
+                throw new ArgumentOutOfRangeException("x");
+
             if (x < 20)
             {
                 return 20;
--- Mathemagic/Mathemagic.Tests/AddTests.cs
+++ Mathemagic/Mathemagic.Tests/AddTests.cs
@@ -1,19 +1,30 @@
 using Mathemagic.Core;
 using NUnit.Framework;
+using System;
 
 namespace Mathemagic.Tests
 {
     public class AddTests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
         [Test]
         public void Test1()
         {
             Assert.AreEqual(5, Operations.Add(2, 3));
+        }
+
+        /// <summary>
+        /// The logic of this test is impeccable.
+        /// </summary>
+        [Test]
+        public void Test2()
+        {
+            Assert.AreEqual(5, Operations.Add(3, 3));
+        }
+
+        [Test] 
+        public void Test3()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => Operations.Add(-1, 3));
         }
     }
 }
